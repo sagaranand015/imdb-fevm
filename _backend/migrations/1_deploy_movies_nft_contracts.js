3adecde8f074f488afd15cb1109ea3358f1247d8
Sagar Anand <sagar.anand015@gmail.com>
2022-11-20 02:38:24 +0530

    removing everything for hardhat project

--- _backend/migrations/1_deploy_movies_nft_contracts.js
+++
@@ -1,9 +0,0 @@
-const moviesNft = artifacts.require("MoviesNFT");
-
-const moviesNftOwner = "0xf4267F20B463421D2cF3db534491b7920F79Ac4F";
-
-module.exports = async function (deployer) {
-    await deployer.deploy(moviesNft).then(function (resp) {
-        console.log("Movies NFT Contract Deployed on Wallaby at: ", resp.address);
-    });
-};
