4fc5e4faf9f01744c5974f4e8005c1d615c05b90
Sagar Anand <sagar.anand015@gmail.com>
2022-11-20 02:36:30 +0530

    removing everything for hardhat project

--- demo-app/src/constants.js
+++
@@ -1,4 +0,0 @@
-export const MOVIES_NFT_CONTRACT_ADDRESS = "0xA3975e3Fd82BD9ccD8dc33Af97386779C514411c";
-export const NFT_STORAGE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDA2NEJDRWQ5ZjY1YzM2N2QxM2MzNkVCNDhGMzdlMjFGYjRCNDU3RDkiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY2Nzg1MjAxNjMyOCwibmFtZSI6InRlc3QwMSJ9.LiE7elIFIwLYbHqgsoxwO9Lk-m112x6GFp5nzgYQ_EQ";
-
-export const DEMO_MOVIE_IMAGE = "ipfs://bafkreieddeiyxsyk7x6twdshl74fpfvcro5riwj4vfxo5zty3pdfzvf73m";
